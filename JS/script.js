0d8790b08cd3e51a78b0e716302c7736ba64eaf2
luizneto375 <114784434+luizneto375@users.noreply.github.com>
2023-11-02 13:40:45 -0300

    add emojis

--- JS/script.js
+++ JS/script.js
@@ -6,14 +6,16 @@ function atualizar() {
     // Essa é a função responsavel por gerar um número aleatorio
 
     numeroParaEncontrar = parseInt(Math.random() * 100);//OBS: esse é um metodo nativo do JS, Ele faz gerar um número entre 0 e 1, então vamos usar ele para poder gerar esse número entre 0 a 100, apenas temos que multiplicar esse metodo por 100;
+    console.log(numeroParaEncontrar)
 }
 
 function verificarNumero() {
     let acerto = document.querySelector("#acertar").value;
 
     if(acerto > 100 || acerto < 1) {
-        document.querySelector("h2").innerHTML = "Número invalido!!!";
+        document.querySelector("h2").innerHTML = "&#128680;Ops!!! Número invalido!!!";
         return;
+       
     }
 
 if(acerto > numeroParaEncontrar) {
@@ -25,7 +27,7 @@ if(acerto > numeroParaEncontrar) {
     tentativas++;
     document.querySelector("h2").innerHTML = "&#128550; tente um número MAIOR!!";
 } else {
-    document.querySelector("h2").innerHTML = "&#128526; Parabéns você Acertou!!!" + " Você errou " + tentativas + " vezes ";
+    document.querySelector("h2").innerHTML = "Parabéns você Acertou!!! &#127881;	&#128640;	&#128640;" + " Você errou " + tentativas + " vezes &#128526; ";
 }
 
 }
